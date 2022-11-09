9b71f004a6f9ecce35fded0b74eb265b6d9238a3
Jon Beck <jonnydbeck@gmail.com>
2022-11-09 18:38:52 -0500

    UI Skeleton Created

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function start(){
     console.log("░░░██║░░░██║░░██║██║░░██║╚█████╔╝██║░╚██╗███████╗██║░░██║")
     console.log("░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝\n");
 
+    //Function's first calling
+    list();
+}
+
+//Honestly I found listed prompts, if statements, and recursive functions the easiest
+function list(){
     //list prompt
     inquirer.prompt([
         {
@@ -28,6 +34,94 @@ function start(){
           loop: true
         },
     ]).then(answers => {
-        console.info(answers.menu, " has been selected")
+        //if statement for each answer
+        if (answers.menu == 'Veiw Table'){
+
+            //Table view prompt
+            inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'vMenu',
+                  message: 'Which table would you like to view?',
+                  choices: ['Veiw Department Table', 'Veiw Role Table', 'Veiw Employee Table', 'Back'],
+                  loop: true
+                },
+            ]).then(answers => {
+                if (answers.vMenu == 'Veiw Department Table'){
+
+                } else if (answers.vMenu == 'Veiw Role Table'){
+
+                } else if (answers.vMenu == 'Veiw Employee Table'){
+
+                } else if (answers.vMenu == 'Back'){
+                    list();
+                } else {
+                    //Just in case
+                    console.error("An unknown error has occured")
+                    list();
+                }
+            });
+
+        } else if (answers.menu == 'Add to Table'){
+
+            //Table view prompt
+            inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'aMenu',
+                  message: 'Which table would you like to view?',
+                  choices: ['Add to Department Table', 'Add to Role Table', 'Add to Employee Table', 'Back'],
+                  loop: true
+                },
+            ]).then(answers => {
+                if (answers.aMenu == 'Add to Department Table'){
+
+                } else if (answers.aMenu == 'Add to Role Table'){
+
+                } else if (answers.aMenu == 'Add to Employee Table'){
+
+                } else if (answers.aMenu == 'Back'){
+                    list();
+                } else {
+                    //Just in case
+                    console.error("An unknown error has occured")
+                    list();
+                }
+            });
+            
+        } else if (answers.menu == 'Update from Table'){
+
+            //Table view prompt
+            inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'uMenu',
+                  message: 'Which table would you like to view?',
+                  choices: ['Update Department Table', 'Update Role Table', 'Update Employee Table', 'Back'],
+                  loop: true
+                },
+            ]).then(answers => {
+                if (answers.uMenu == 'Update Department Table'){
+
+                } else if (answers.uMenu == 'Update Role Table'){
+
+                } else if (answers.uMenu == 'Update Employee Table'){
+
+                } else if (answers.uMenu == 'Back'){
+                    list();
+                } else {
+                    //Just in case
+                    console.error("An unknown error has occured")
+                    list();
+                }
+            });
+            
+        } else if (answers.menu == 'Quit'){
+            console.info("Quitting...")
+        } else {
+            //Just in case
+            console.error("An unknown error has occured")
+            list();
+        }
     });
 }
